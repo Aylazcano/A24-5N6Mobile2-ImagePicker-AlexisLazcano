52751f95a185fce373ffd679ef9ce2b26058116e
Alexis Lazcano <0370102@cegepmontpetit.ca>
2024-10-23 17:13:41 -0400

    PROGRES: Layout, image picker

--- image_background/lib/main.dart
+++ image_background/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Image Background',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Image Background Page'),
     );
   }
 }
@@ -55,17 +58,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final picker = ImagePicker();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  var _imageFile = null;
+
+
+
+  Future getImage() async {
+    print("ouverture du selecteur d'image");
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      print("l'image a ete choisie ${pickedFile.path}");
+      _imageFile = File(pickedFile.path);
+      setState(() {});
+    } else {
+      print('Pas de choix effectue.');
+    }
   }
 
   @override
@@ -104,21 +114,24 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: [
+            Image.file(File(_imageFile)),
+          TextField(
+          controller: _usernameController,
+          decoration: const InputDecoration(labelText: 'Username'),
+        ),
+          TextField(
+          controller: _passwordController,
+          decoration: const InputDecoration(labelText: 'Password'),
+          obscureText: true,
+        ),
+        ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: getImage,
+        tooltip: 'Select background image',
+        child: const Icon(Icons.search),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
